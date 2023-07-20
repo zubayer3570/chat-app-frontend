ba72658a160698fba2c2c89edde4edcbeb726396
zubayer <zubayer3570@gmail.com>
2023-07-20 13:45:24 +0600

    fixed-and-optimized-2

--- src/features/conversationSlice.js
+++ src/features/conversationSlice.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
 export const newConversationThunk = createAsyncThunk("newConversationThunk", async (newConversation) => {
-    const res = await axios.post("http://localhost:5000/add-conversation", newConversation)
+    const res = await axios.post("https://chat-app-pzz6.onrender.com/add-conversation", newConversation)
     return res.data
 })
 
